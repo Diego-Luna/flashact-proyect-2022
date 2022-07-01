b336a852f4a715b62d5d4f5df1bbfe23df2478fc
Diego-Luna <diegofcolunalopez@gmail.com>
2022-06-30 19:59:12 -0500

    cambio de imgs y texto

--- documentation-platform/src/app/app.module.ts
+++ documentation-platform/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
@@ -20,7 +22,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- documentation-platform/src/app/home/home.component.ts
+++ documentation-platform/src/app/home/home.component.ts
@@ -1,9 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+
+// import {
+//   trigger,
+//   state,
+//   style,
+//   animate,
+//   transition,
+//   // ...
+// } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
+  // animations: [
+  //   // los disparadores de animación van aquí
+  //   trigger('openClose', [
+  //     // ...
+  //     state('open', style({
+  //       // height: '200px',
+  //       opacity: 1,
+  //       backgroundColor: 'yellow'
+  //     })),
+  //     state('closed', style({
+  //       // height: '100px',
+  //       opacity: 0.8,
+  //       backgroundColor: 'blue'
+  //     })),
+  //     transition('open => closed', [
+  //       animate('2s')
+  //     ]),
+  //     transition('closed => open', [
+  //       animate('1.5s')
+  //     ]),
+  //   ]),
+  // ]
 })
 export class HomeComponent implements OnInit {
 
@@ -19,6 +50,26 @@ export class HomeComponent implements OnInit {
   url_img_section_4: string = "./assets/pasos/1_calibracion";
   url_img_section_5: string = "./assets/pasos/1_calibracion";
 
+  pasos_text = {
+    seccion1: [
+      `Para programar el arduino usaremos el Arduino IDE, puedes usar la versión web o puedes descargarlo en tu computadora.\nAbre el programa y ve a Herramientas > Placas > Gestor de tarjetas,después busca “Arduino MKR” e instálalo. Conecta el módulo del sensor de PH de la siguiente manera:\n- Cable rojo a 5v\n- Cable megro a GND\n- Cable azul a A0
+      `,
+      `Perfecto, ahora coloca el sensor en un líquido de ph = 7.00.
+      Abre el código “Calibracion_de_Ph”, conecta el arduino a la computadora y selecciona la placa, ve a Herramientas > placas y selecciona el puerto, por último sube el código y abre el monitor serial, aqui veras el valor del sensor, ahora deberás mover el potenciómetro para bajar o subir el valor del sensor para ser el de tu líquido de muestreo en este caso ph = 7.00.
+      `,
+      `También para finalizar debes calcular la diferencia del valor medido y el correcto, para colocarlo en el valor “#define Offset 0.00”. Por ejemplo, si el valor medio es de 9 y el correcto es 7, la diferencia es 2, asi que modificas “#define Offset -2.00”.`,
+    ],
+    seccion2: [
+      "Conecta todos los cables rojos o positivos de los sensores y conectalos en el positivo de la batería (5V).",
+      "Depsues conecta los cables negros o negativos y conectalos al lado negativo de la bateria (GND).",
+      "Ahora conecta los cables de datos de los sensores, temperatura conectalo a D2, pH a A0, TDS a A1 y Turbiedad a A2.",
+      "Por último conecta el arduino a la batería a 3.7v.",
+    ]
+  }
+
+  text_section_1 = this.pasos_text.seccion1[0];
+  text_section_2 = this.pasos_text.seccion2[0];
+
   constructor() { }
 
   ngOnInit(): void {
@@ -29,9 +80,11 @@ export class HomeComponent implements OnInit {
     switch (seccion) {
       case "1":
         this.status_section_1 = nuevo_estado;
+        this.changeText("1", nuevo_estado);
         break;
       case "2":
         this.status_section_2 = nuevo_estado;
+        this.changeText("2", nuevo_estado);
         break;
       case "3":
         this.status_section_3 = nuevo_estado;
@@ -46,6 +99,7 @@ export class HomeComponent implements OnInit {
         console.log("Error en seccion: " + seccion + " en nuevo_estado: "+ nuevo_estado);
         break;
     }
+    console.log("Click");
   }
 
   getImageSections(seccion:string, img_url:string):string{
@@ -68,6 +122,19 @@ export class HomeComponent implements OnInit {
       default:
         console.log("Error en seccion: " + seccion + " en img_url: "+ img_url);
         return (`${img_url}/`)
+        break;
+    }
+  }
+
+  changeText(seccion:string, text_number:number){
+    switch (seccion) {
+      case "1":
+        this.text_section_1 = this.pasos_text.seccion1[text_number - 1];
+        break;
+      case "2":
+        this.text_section_2 = this.pasos_text.seccion2[text_number - 1];
+        break;
+      default:
         break;
     }
   }
